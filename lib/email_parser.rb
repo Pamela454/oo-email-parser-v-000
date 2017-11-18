7e6776e3d7cff0d1da5c14936d4542544e1a9e31
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 19:14:49 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@ class EmailParser
   attr_accessor :email #set and get a list of emails
 
 
-  def parse(email) #take in emails
+  def self.parse(email) #take in emails
     #split emails based on commas or whitespace
     email.split("," " ")
   end
